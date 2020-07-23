48c99bf529ff3d466efcc540de6f579b8b4526b4
qrovic <66991791+qrovic@users.noreply.github.com>
2020-07-24 00:48:43 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,7 +29,7 @@ TARGET_NO_BOOTLOADER := true
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/boot.img-zImage
 BOARD_MKBOOTIMG_ARGS := --base 0x40078000 --ramdisk_offset 0x14f88000 --second_offset 0x00f00000 --tags_offset 0x13f88000
 
 # system.prop
